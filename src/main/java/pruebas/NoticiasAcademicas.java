ef6a95cb8c987e329888c035408f17fc94d14f3a
Manuel Florez <manuelflorezw@gmail.com>
2019-05-24 11:54:41 -0500

    Initial commit

---
+++ src/main/java/pruebas/NoticiasAcademicas.java
@@ -0,0 +1,59 @@
+package pruebas;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class NoticiasAcademicas {
+
+    private String url = "https://divisist2.ufps.edu.co/";
+    // xpath contenedor principal
+    private By div = By.xpath("/html/body/div[1]/div[2]/div[2]");
+
+    private WebDriver driver;
+
+    private By header = By.tagName("header");
+    private By h1 = By.tagName("h1");
+    private By a = By.tagName("a");
+    private By p = By.tagName("p");
+
+    public NoticiasAcademicas(WebDriver driver){
+        this.driver = driver;
+    }
+
+    public String getTitulo(){
+        WebElement element = driver.findElement( div );
+        element = element.findElement( header );
+        element = element.findElement( h1 );
+
+        return element.getText();
+    }
+
+    public List<WebElement> noticias(){
+        WebElement element = driver.findElement( div );
+        element = element.findElement( By.tagName("div") );
+        element = element.findElement( By.tagName("div") );
+
+        List<WebElement>lista = element.findElements( p );
+
+        System.out.println(lista.size());
+
+        for (WebElement e :lista) {
+            System.out.println(e.getText());
+            System.out.println("---------------------------");
+        }
+
+        return lista;
+    }
+
+    public void cerrarNoticias(){
+        WebElement element = driver.findElement( div );
+        element = element.findElement( header );
+        element = element.findElement( a );
+
+        element.click();
+    }
+
+}
---
+++ src/main/java/pruebas/Prueba.java
@@ -0,0 +1,25 @@
+package pruebas;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Prueba {
+
+    private static WebDriver driver = new ChromeDriver();
+
+    public static void main(String[] args) {
+
+        driver.get("https://divisist2.ufps.edu.co/");
+
+        NoticiasAcademicas n = new NoticiasAcademicas(driver);
+        System.out.println( n.getTitulo() );
+        n.noticias();
+        n.cerrarNoticias();
+
+        driver.quit();
+
+    }
+
+
+
+}
